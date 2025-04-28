26b5f82ba51acd419e1b90f3028fdb2ec513ad07
Davi <davilima7002@gmail.com>
2025-04-28 18:08:31 -0300

    Finalização da Caculadora de 3 operações

--- Projeto.py
+++ Projeto.py
@@ -9,5 +9,19 @@ while c != 0:
     if c == 1:
         a = int(input('Primeiro valor: '))
         b = int(input('Segundo valor: '))
-        print('A soma de {} + {} = {}'.format(a,b,a+b))
+        print('A soma de {} + {} = {}'.format(a, b, a+b))
         break
+    elif c == 2:
+        a = int(input('Primeiro valor: '))
+        b = int(input('Segundo valor: '))
+        if a > b:
+            print('A subtração de {} - {} = {}'.format(a, b, a-b))
+        else:
+            print('A subtração de {} - {} = {}'.format(b, a, b-a))
+        break
+    elif c == 3:
+        a = int(input('Primeiro valor: '))
+        b = int(input('Segundo valor: '))
+        print('A multiplicação de {} * {} = {}'.format(a, b, a*b))
+        break
+print('\033[31mFim do programa.\033[m.')
